1015aa1d1cc96ca68eea36b42463b67c8e32d7a9
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-04 19:53:22 +0200

    Added flowbite JS script

--- pages/_app.js
+++ pages/_app.js
@@ -12,8 +12,8 @@ class MyApp extends App {
         <Head>
           <title>ElPepe Streams</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
           <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
         </Head>
         <Component {...pageProps} />
         <NavbarComponent />
