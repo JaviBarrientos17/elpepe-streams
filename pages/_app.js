44ce20da692f16e42af87314b0f408d6d684be43
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-04 19:57:27 +0200

    Added next/script

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import NavbarComponent from "../components/NavbarComponent";
+import Script from "next/script";
 
 class MyApp extends App {
   render() {
@@ -13,7 +14,7 @@ class MyApp extends App {
           <title>ElPepe Streams</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
+          <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js" />
         </Head>
         <Component {...pageProps} />
         <NavbarComponent />
