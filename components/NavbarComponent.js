8bd6944da05c1ac052714a9407f9822aa0bf78fc
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-04 11:10:14 +0200

    Uninstalled Bulma

--- components/NavbarComponent.js
+++ components/NavbarComponent.js
@@ -1,35 +1,7 @@
 import React from "react";
-import { Navbar, Text, Input } from "@nextui-org/react";
-import { Layout } from "./Layout.js";
-import { Logo } from "./Logo.js";
 
 export default function NavbarComponent() {
-  const [variant, setVariant] = React.useState("default");
-  const [activeColor, setActiveColor] = React.useState("primary");
-
   return (
-    <Layout>
-      <Navbar isBordered="no" variant="static" activeColor={setActiveColor}>
-        <Navbar.Brand>
-          <Logo />
-          <Text color="inherit" hideIn="xs">
-            ElPepe Streams
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content>
-          {/* // TODO CHECK THEME TOGGLE INPUT FROM THE test.html */}
-          {/* // TODO OR MAKE A DIFFERENT THEME TOGGLE IN THE NAVBAR */}
-          {/* // TODO OR CHANGE THE COLOR OF THE NAVBAR TO DARK */}
-
-          {/* // TODO UNINSTALL NEXT.UI AND INSTALL BULMA --> https://bulma.io/documentation/components/navbar/ */}
-
-        </Navbar.Content>
-        <Navbar.Content activeColor={activeColor} hideIn="xs" variant={variant}>
-          <Navbar.Link href="/repeticiones">Repeticiones</Navbar.Link>
-          <Navbar.Link href="#">Peliculas</Navbar.Link>
-          <Navbar.Link href="#">Series</Navbar.Link>
-        </Navbar.Content>
-      </Navbar>
-    </Layout>
+    <></>
   );
 }
