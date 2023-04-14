76220990d49fc4f0be4b80030708caebd63c7204
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-14 15:34:22 +0200

    Added homepage gif

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,18 @@ export default function HomeComponent() {
         <title>El Pepe Streams</title>
       </Head>
 
-      <img
-        src="https://bit.ly/3zJ9veb"
-        className="w-full object-cover"
-        alt="background"
-      />
+      <div class="flex min-h-screen w-full justify-center items-center">
+        <img
+          src="https://drive.google.com/uc?export=view&id=146ksotIL9W2u_MmIfS_fyF8LvVgYTXuU"
+          alt="Homepage img"
+          class="h-full max-w-full object-contain object-center"
+        />
+      </div>
 
       <div className="relative z-10 flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold text-white text-center sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl mt-8 sm:mt-16">
           ElPepe Streams
         </h1>
-
         <p className="mt-8 text-lg text-white text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
           Bienvenidos a ElPepe Streams, tu fuente de entretenimiento en línea.
           Aquí encontrarás una amplia selección de películas y series para ver
