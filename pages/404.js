dbec871ecf357443f25e405f78e8221575425c63
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-18 16:55:43 +0200

    Added custom 404 error page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import FooterComponent from "../components/FooterComponent";
+
+export default function Custom404() {
+  return (
+    <>
+      <img
+        src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDQxNDM4ZDMxZjU5ZWJkZjkzZGQ2NDkwODcxMWY5OWRmMDBiMDRhOCZjdD1z/TLeLKUdIc1tvAxb7ab/giphy.gif"
+        class="mx-auto w-100 h-100"
+        alt="Work in progress img"
+      />
+
+      <FooterComponent />
+    </>
+  );
+}
