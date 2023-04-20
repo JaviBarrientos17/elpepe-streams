da302f8b7099ae6100424392697f96024b241c0b
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-20 17:19:38 +0200

    ClassName 404.js

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ export default function Custom404() {
     <>
       <img
         src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDQxNDM4ZDMxZjU5ZWJkZjkzZGQ2NDkwODcxMWY5OWRmMDBiMDRhOCZjdD1z/TLeLKUdIc1tvAxb7ab/giphy.gif"
-        class="mx-auto w-100 h-100"
+        className="mx-auto w-100 h-100"
         alt="Work in progress img"
       />
 
