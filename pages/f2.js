c2934d99cee726b9736e4ef5e1218ff89eab9e39
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-04-21 12:55:54 +0200

    <img> to next/image F2

--- pages/f2.js
+++ pages/f2.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import FooterComponent from "../components/FooterComponent";
 import f2Data from "../data/F2.json";
 
@@ -20,10 +21,12 @@ export default function F2() {
                   <h2 className="title text-lg font-bold my-4">
                     {f2Mapped.title}
                   </h2>
-                  <img
+                  <Image
                     src={f2Mapped.img}
                     alt={f2Mapped.title}
-                    className="object-contain w-full"
+                    width={800}
+                    height={600}
+                    className="object-contain"
                   />
                   <p className="description mt-2 mb-2 text-gray-700 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl leading-6 sm:leading-7 md:leading-8 lg:leading-9 xl:leading-10 whitespace-normal">
                     {f2Mapped.description}
