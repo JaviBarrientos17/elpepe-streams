6c6098b2bb763cf442a16acf70ff1abc8e793a66
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-05-03 13:01:34 +0200

    Installed pkgs for csvToJson.js

---
+++ csvToJson.js
@@ -0,0 +1,41 @@
+const csvFilePath = "C:/Users/javie/Downloads/peliculas_subidas.csv";
+const papaparse = require("papaparse");
+const fs = require("fs");
+
+// Lista con los nombres de las columnas que se desean convertir en el archivo JSON
+const columnas = ["Content", "link"];
+
+fs.readFile(csvFilePath, "utf-8", (err, data) => {
+  if (err) throw err;
+
+  // Se utiliza la librería "papaparse" para leer el archivo CSV
+  const jsonObj = papaparse.parse(data, { header: true }).data;
+
+  // Se crea un nuevo objeto JSON que contiene las columnas especificadas
+  const jsonData = jsonObj.map((obj) => {
+    const data = {};
+    columnas.forEach((col) => {
+      if (col === "downloadUrls") {
+        const urls = obj.Content
+          ? obj.Content.match(/(http|https):\/\/[^\s]+/g)
+          : [];
+        data[col] = urls;
+      } else {
+        if (col in obj) {
+          data[col] = obj[col].replace(/"/g, '\\"');
+        }
+      }
+    });
+    return data;
+  });
+
+  // Se guarda el objeto JSON generado en un archivo
+  fs.writeFile(
+    "C:/Users/javie/Downloads/archivo.json",
+    JSON.stringify(jsonData),
+    (err) => {
+      if (err) throw err;
+      console.log("Archivo JSON guardado exitosamente!");
+    }
+  );
+});
