a92a20884b9fa01fc03a51e7daa073c4c7c203b5
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-05-08 10:13:47 +0200

    Modified styles MyVideoPlayer

--- pages/videoPlayer.js
+++ pages/videoPlayer.js
@@ -3,7 +3,7 @@ import React from "react";
 const MyVideoPlayer = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <div className="max-w-screen-lg w-full mx-4 sm:mx-8 md:mx-16 lg:mx-32 xl:mx-64">
+      <div className="w-full h-full max-w-4xl mx-auto">
         <iframe
           title="Player"
           className="w-full h-96 md:h-500px"
