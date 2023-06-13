c8ab2717ec57dfc816cea18684e522cde18cb0df
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-06-13 09:48:19 +0200

    Resolving bug hamburger menu navbar phone

--- components/NavbarComponent.js
+++ components/NavbarComponent.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NavbarComponent() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar border-gray-200 dark:bg-gray-900">
       <div className="navbarContent max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -16,7 +22,8 @@ export default function NavbarComponent() {
           type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
-          aria-expanded="false"
+          aria-expanded={isMenuOpen ? "true" : "false"}
+          onClick={toggleMenu}
         >
           <span className="sr-only">Menú principal</span>
           <svg
@@ -33,7 +40,12 @@ export default function NavbarComponent() {
             ></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
           <ul className="navbarLinks font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
