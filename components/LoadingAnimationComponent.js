5dce6798dfdafa0dfcdb3dc5f7b3bff432ba084f
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-06-29 10:24:28 +0200

    Added LoadingAnimationComponent & styles

---
+++ components/LoadingAnimationComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoadingAnimationComponent = () => {
+  return (
+    <>
+      <span className="loader">
+        <span className="loader_ball"></span>
+        <span className="loader_ball"></span>
+        <span className="loader_ball"></span>
+      </span>
+    </>
+  );
+};
+
+export default LoadingAnimationComponent;
